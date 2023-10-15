15578fd9dab525a0ce48868b2e66e4d66a5f8653
Bruno <bpires.siqueira@gmail.com>
2023-10-15 11:15:14 -0300

    feat: menu mobile all pages

---
+++ js/menu_mobile.js
@@ -0,0 +1,21 @@
+function menuMobile() {
+    const btnMobile = document.getElementById('btn_mobile');
+
+    function toggleMenu(event) {
+        if (event.type === 'touchstart') event.preventDefault();
+        const nav = document.querySelector('.nav');
+        nav.classList.toggle('active');
+        const active = nav.classList.contains('active');
+        event.currentTarget.setAttribute('aria-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+        }
+    }
+
+    btnMobile.addEventListener('click', toggleMenu);
+    btnMobile.addEventListener('touchstart', toggleMenu);
+}
+
+menuMobile();
