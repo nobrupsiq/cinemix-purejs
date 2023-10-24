1054688fa504f9b26116e167c8782f68b39b2389
Bruno <bpires.siqueira@gmail.com>
2023-10-24 09:48:36 -0300

    fix: bugs a resolver

--- js/filmes.js
+++ js/filmes.js
@@ -1,16 +1,35 @@
-const playButton = document.querySelectorAll('.trailer_play');
-const trailerContainer = document.querySelector('.modal_trailer_container');
-const fecharModal = document.querySelector('.fechar_modal');
-const modalTrailer = document.querySelector('.modal_trailer');
+const modalTrailer = () => {
+    const playButton = document.querySelectorAll('.trailer_play');
+    const trailerContainer = document.querySelector('.modal_trailer_container');
+    const fecharModal = document.querySelector('.fechar_modal');
+    const modalTrailer = document.querySelector('.modal_trailer');
 
-playButton.forEach((item) => {
-    item.addEventListener('click', () => {
-        trailerContainer.style.visibility = 'visible';
-        modalTrailer.classList.add('modal_trailer_ativo');
+    playButton.forEach((item) => {
+        item.addEventListener('click', () => {
+            trailerContainer.style.visibility = 'visible';
+            modalTrailer.classList.add('modal_trailer_ativo');
+        });
     });
-});
 
-fecharModal.addEventListener('click', () => {
-    trailerContainer.style.visibility = 'hidden';
-    modalTrailer.classList.remove('modal_trailer_ativo');
-});
+    fecharModal.addEventListener('click', () => {
+        trailerContainer.style.visibility = 'hidden';
+        modalTrailer.classList.remove('modal_trailer_ativo');
+    });
+};
+modalTrailer();
+
+const adicionarFilme = () => {
+    const modalAdd = document.querySelector('.form_filmes_add');
+    const addFilme = document.querySelector('.addFilme');
+    const addFilmeContainer = document.querySelector(
+        '.modal_filmes_container_add',
+    );
+
+    addFilme.addEventListener('click', (event) => {
+        event.preventDefault();
+        modalAdd.classList.add('ativo');
+        addFilmeContainer.classList.add('ativo');
+    });
+};
+
+adicionarFilme();
