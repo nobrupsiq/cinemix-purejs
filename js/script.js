afed185a7a78f0f1efd6f7aebea5397acca869c4
Bruno <bpires.siqueira@gmail.com>
2023-11-25 22:30:57 -0300

    feat: home scroll top

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ const slide = new SlideNav('.slide', '.slide_wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
 slide.addControl();
+
+// Scroll para o topo
+
+function scrollTop() {
+    if (window.scrollY >= 760) {
+        document.querySelector('.moveTop').classList.add('ativo');
+    }
+    if (window.scrollY < 760) {
+        document.querySelector('.moveTop').classList.remove('ativo');
+    }
+}
+
+window.addEventListener('scroll', scrollTop);
+
+function goTop() {
+    window.scrollTo(0, 0);
+}
+
+const moveTop = document.querySelector('.moveTop img');
+moveTop.addEventListener('click', goTop);
