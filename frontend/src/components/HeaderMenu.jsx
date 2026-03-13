c834d27ce3201751bfcfc97dd9d79ab9f5c126dd
bruno <bpires.siqueira@gmail.com>
2026-03-13 18:57:13 -0300

    update: headerMenu

--- frontend/src/components/HeaderMenu.jsx
+++ frontend/src/components/HeaderMenu.jsx
@@ -1,35 +1,34 @@
 const HeaderMenu = () => {
   return (
-    <div className="flex items-center justify-between p-page tracking-[2px] sticky top-0 z-100">
-      <div>
-        <h1 className="text-4xl uppercase font-display text-white">
-          Cine<span className="text-mu">Mix•</span>
-        </h1>
+    <div className="absolute top-0 left-0 w-full z-50">
+      <div className="flex items-center justify-between p-page tracking-[2px] container mx-auto">
+        <div>
+          <h1 className="text-4xl uppercase font-display text-white">
+            Cine<span className="text-mu">Mix•</span>
+          </h1>
+        </div>
+        <nav className="font-body text-xs font-bold text-gr">
+          <ul className="flex gap-10 uppercase items-center">
+            <li className="link-underline after:duration-300 pt-2">
+              <a href="">Início</a>
+            </li>
+            <li className="link-underline after:duration-300 pt-2">
+              <a href="">Filmes</a>
+            </li>
+            <li className="link-underline after:duration-300 pt-2">
+              <a href="">Combos</a>
+            </li>
+            <li className="link-underline after:duration-300 pt-2">
+              <a href="">Programação</a>
+            </li>
+            <li>
+              <button className="btn-primary">
+                <span>Ingressos</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="font-body text-xs font-bold text-gr">
-        <ul className="flex gap-10 uppercase items-center">
-          <li className="link-underline after:duration-300 pt-2">
-            <a href="">Início</a>
-          </li>
-          <li className="link-underline after:duration-300 pt-2">
-            <a href="">Filmes</a>
-          </li>
-          <li className="link-underline after:duration-300 pt-2">
-            <a href="">Combos</a>
-          </li>
-          <li className="link-underline after:duration-300 pt-2">
-            <a href="">Programação</a>
-          </li>
-          <li>
-            <a
-              className="bg-mu pt-3 pb-3 pr-6 pl-6 rounded-xs text-black"
-              href=""
-            >
-              Ingressos
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,10 +4,39 @@ import HeaderMenu from "./HeaderMenu";
 const Hero = () => {
   return (
     <section
-      className="min-h-screen bg-cover bg-center"
+      className="min-h-screen flex flex-col justify-center bg-cover bg-center"
       style={{ backgroundImage: `url(${bgHome})` }}
     >
       <HeaderMenu />
+      <div className="p-page relative ">
+        <span className="badge-now">• Em cartaz agora</span>
+
+        {/* GLOW */}
+        <div className="absolute left-40 top-24 w-72 h-72 bg-yellow-400/40 blur-[280px]" />
+        <div className="absolute left-56 top-32 w-56 h-56 bg-yellow-300/30 blur-[140px]" />
+
+        <h2 className="uppercase text-9xl text-stroke font-display leading-none">
+          Cinema
+        </h2>
+        <h1 className="font-display uppercase text-[9rem] leading-[0.85] text-mu font-bold">
+          Reimaginado.
+        </h1>
+        <div className="text-gr w-105 mt-8 mb-8 font-body">
+          <p>Uma experiência cinematográfica que vai além da tela.</p>
+          <p>
+            Escolha seu filme, reserve seu lugar e mergulhe no universo do
+            cinema.
+          </p>
+        </div>
+        <nav className="flex gap-4">
+          <button className="btn-primary">
+            <span>Ver programação</span>
+          </button>
+          <button className="btn-secondary">
+            <span>Explorar filmes</span>
+          </button>
+        </nav>
+      </div>
     </section>
   );
 };
